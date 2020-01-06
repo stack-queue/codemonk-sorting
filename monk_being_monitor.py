9a27c6f70f7f2fb0589b6edfd9beb402d28f93e0
Stack Queue <brightside1399@gmail.com>
2020-01-06 22:20:32 +0530

    Update monk_being_monitor.py

--- monk_being_monitor.py
+++ monk_being_monitor.py
@@ -6,33 +6,23 @@ for i in range(n_testcases):
     for height in input().split():
         heights.append(int(height))
     
-    h1_heights = []
-    for height in heights:
-        h1_heights.append(height)
+    set_heights = set(heights)
+    
+    h1, h2 = None, None
+    for set_height in set_heights:
+        temp = heights.count(set_height)
         
-    h1 = None
-    for height in h1_heights:
-        temp = h1_heights.count(height)
         if h1 == None:
             h1 = temp
         elif h1 > temp:
             h1 = temp
-            for i in range(temp):
-                h1_heights.remove(height)
-    
-    h2_heights = []
-    for height in heights:
-        h2_heights.append(height)
-
-    h2 = None
-    for height in h2_heights:
-        temp = h2_heights.count(height)
         if h2 == None:
             h2 = temp
         elif h2 < temp:
             h2 = temp
-            for i in range(temp):
-                h2_heights.remove(height)
+        
+        for i in range(temp):
+            heights.remove(set_height)
     
     if abs(h1 - h2) == 0:
         print(-1)
